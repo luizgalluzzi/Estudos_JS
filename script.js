5a0b54ff7a445b1e362af20251278ec15fc92197
Luiz galluzzi <luizgalluzzi@yahoo.com.br>
2022-11-12 11:22:38 -0300

    exercicio #3 #24

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+// Elementos
+const input = document.querySelector('input');
+const lista = document.querySelector('ul');
 
-let numero = 1;
-
-while(numero<=100){
-    console.log (numero);
-    numero++;
+// Funcoes
+function handleKeyUp(e) {
+    if(e.key === 'Enter') {
+        const newLi = document.createElement('li'); //cria uma nova lista
+        newLi.innerHTML = input.value; //acrescenta o novo valor
+        lista.appendChild(newLi); 
+        input.value = ''; //zerar o campo
+    }
 }
+
+// Eventos
+input.addEventListener('keyup',handleKeyUp);
 
